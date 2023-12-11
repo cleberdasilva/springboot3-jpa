c1aafa643bc51eaa1a0b9ebfbec988c76cccb1cf
cleberdasilva <clebers.supertux@gmail.com>
2023-12-11 14:39:30 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/pingosystem/course/entities/Product.java
+++ src/main/java/com/pingosystem/course/entities/Product.java
@@ -8,8 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -27,7 +29,10 @@ public class Product implements Serializable {
 	
 	//o mesmo produto não pode ter a mesma categoria mais de uma vez, por isso usar Set
 	//tem que ser instaciada para ser vazia e não nula
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_prodcut_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {}
