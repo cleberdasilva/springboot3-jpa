fab76e6abbdd101e4bbfa54dc91847c203425807
cleberdasilva <clebers.supertux@gmail.com>
2023-12-11 16:22:35 -0300

    OrderItem, many-to-many association with extra attributes - Part II

--- src/main/java/com/pingosystem/course/entities/Order.java
+++ src/main/java/com/pingosystem/course/entities/Order.java
@@ -2,6 +2,8 @@ package com.pingosystem.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pingosystem.course.entities.enums.OrderStatus;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,7 +32,8 @@ public class Order implements Serializable  {
 		
 		private Integer orderStatus;
 		
-		
+		@OneToMany(mappedBy = "id.order")
+		private Set<OrderItem> items = new HashSet<>();
 		
 		@ManyToOne
 		@JoinColumn(name = "client_id")
@@ -80,6 +84,10 @@ public class Order implements Serializable  {
 			this.client = client;
 		}
 
+		public Set<OrderItem> getItems(){
+			return items;
+		}
+		
 		@Override
 		public int hashCode() {
 			final int prime = 31;
