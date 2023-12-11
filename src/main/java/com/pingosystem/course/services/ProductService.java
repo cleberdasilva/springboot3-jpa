a3f23c7eeceb93451007697dd8a65c0efce16df8
cleberdasilva <clebers.supertux@gmail.com>
2023-12-11 14:20:43 -0300

    Product

---
+++ src/main/java/com/pingosystem/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.pingosystem.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pingosystem.course.entities.Product;
+import com.pingosystem.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product finById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
