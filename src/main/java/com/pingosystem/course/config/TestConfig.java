eb9ffdd938394a7721e8a1f7786926c3df8102e1
cleberdasilva <clebers.supertux@gmail.com>
2023-12-11 12:02:46 -0300

    Category

--- src/main/java/com/pingosystem/course/config/TestConfig.java
+++ src/main/java/com/pingosystem/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.pingosystem.course.entities.Category;
 import com.pingosystem.course.entities.Order;
 import com.pingosystem.course.entities.User;
 import com.pingosystem.course.entities.enums.OrderStatus;
+import com.pingosystem.course.repositories.CategoryRepository;
 import com.pingosystem.course.repositories.OrderRepository;
 import com.pingosystem.course.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,10 +38,16 @@ public class TestConfig implements CommandLineRunner{
 		Order o1 = new Order(null, Instant.parse("2023-12-12T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2023-12-12T03:42:10Z"), OrderStatus.WAITTING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2023-12-12T15:21:22Z"), OrderStatus.WAITTING_PAYMENT, u1);
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 	
 }
