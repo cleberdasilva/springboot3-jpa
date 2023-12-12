4a69bd83d77648de0ad58789da3dad84d62773c6
cleberdasilva <clebers.supertux@gmail.com>
2023-12-12 17:45:04 -0300

    Exception handling - update

--- src/main/java/com/pingosystem/course/services/UserService.java
+++ src/main/java/com/pingosystem/course/services/UserService.java
@@ -13,41 +13,48 @@ import com.pingosystem.course.repositories.UserRepository;
 import com.pingosystem.course.services.exceptions.DatabaseException;
 import com.pingosystem.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User finById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); //antes era obj.get e retorna erro 500 agora retorna o meu erro
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // antes era obj.get e retorna erro 500 agora
+																			// retorna o meu erro
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
-			repository.deleteById(id);	
+			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id); //antes era getOne
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id); // antes era getOne
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-	
+
 	public void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
